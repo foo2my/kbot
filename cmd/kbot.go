5c9e50ad37d1b283046aa89cec18962ceaf4b362
Serg Fut <sergreat80@gmail.com>
2026-04-15 11:35:31 +0300

    kbot telegram second commit

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -1,19 +1,36 @@
 /*
 Copyright © 2026 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"github.com/spf13/cobra"
+	//"github.com/stianeikeland/go-rpio"
+	telebot "gopkg.in/telebot.v4"
 )
+
+var (
+	// TeleToken bot
+	TeleToken = os.Getenv("TELE_TOKEN")
+)
+
+/*
+// TrafficSignal represents a single traffic light signal
+type TrafficSignal struct {
+	Pin int8
+	On  bool
+}*/
 
 // kbotCmd represents the kbot command
 var kbotCmd = &cobra.Command{
-	Use:   "kbot",
-	Short: "A brief description of your command",
+	Use:     "kbot",
+	Aliases: []string{"start"},
+	Short:   "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
@@ -21,7 +38,80 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("kbot called")
+		//fmt.Println("kbot %s started", appVersion)
+		if TeleToken == "" {
+			log.Fatal("TELE_TOKEN environment variable is not set")
+		}
+
+		fmt.Printf("kbot %s started\n", appVersion)
+		kbot, err := telebot.NewBot(telebot.Settings{
+			URL:    "",
+			Token:  TeleToken,
+			Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+		})
+		if err != nil {
+			log.Fatalf("Please check TELE_TOKEN env variable. %s", err)
+			return
+		}
+		/*
+			if err := rpio.Open(); err != nil {
+				log.Fatalf("Unable to open GPIO: %s", err)
+			}
+			defer rpio.Close()
+
+			// Initialize traffic signals
+			trafficSignals := map[string]*TrafficSignal{
+				"red":   {Pin: 12, On: false},
+				"amber": {Pin: 27, On: false},
+				"green": {Pin: 22, On: false},
+			}
+
+			// Initialize all pins as input
+			for _, signal := range trafficSignals {
+				pin := rpio.Pin(signal.Pin)
+				pin.Input()
+			}
+		*/
+		kbot.Handle(telebot.OnText, func(m telebot.Context) error {
+			//log.Print(m.Message(), telebot.Payload, m.Text())
+			log.Printf(m.Message().Payload, m.Text())
+			payload := m.Message().Payload
+
+			switch payload {
+			case "hello":
+				err = m.Send(fmt.Sprintf("Hello I'm Kbot %s!", appVersion))
+			}
+			return err
+			/*log.Printf("Received message: %s", m.Text())
+			payload := m.Message().Payload
+
+				//return err
+				switch payload {
+				case "hello":
+					return m.Send(fmt.Sprintf("Hello I'm Kbot %s!", appVersion))
+
+				case "red", "amber", "green":
+					signal := trafficSignals[payload]
+					pin := rpio.Pin(signal.Pin)
+
+					if !signal.On {
+						pin.Output()
+						pin.High()
+						signal.On = true
+					} else {
+						pin.Low()
+						pin.Input()
+						signal.On = false
+					}
+
+					return m.Send(fmt.Sprintf("Switched %s light %s", payload, map[bool]string{true: "on", false: "off"}[signal.On]))
+
+				default:
+					return m.Send("Usage: /s red|amber|green")
+				} */
+		})
+
+		kbot.Start()
 	},
 }
 
